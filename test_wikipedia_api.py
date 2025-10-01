71ab2433c4c863101b4f302b68a2b642f6a2ed8c
Dylan <dylboswell@gmail.com>
2025-09-30 19:56:48 -0700

    Adding starter code for script

--- test_wikipedia_api.py
+++ test_wikipedia_api.py
@@ -1,1 +1,20 @@
+import requests
+import geoip2.database
 
+
+url = "https://en.wikipedia.org/w/api.php"
+params = {
+    "action": "query",
+    "list": "recentchanges",
+    "rcprop": "title|user|timestamp|comment|flags",
+    "rclimit": 5,
+    "format": "json"
+}
+
+headers = {"User-Agent": "WikipediaEditTracker/1.0 dboswell@chapman.edu"}
+
+response = requests.get(url, params=params, headers=headers)
+data = response.json()
+
+for change in data["query"]["recentchanges"]:
+    print(change)
