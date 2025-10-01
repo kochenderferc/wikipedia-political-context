c70655f7698cbee3c063e5221f8e8b09523a19f8
Dylan <dylboswell@gmail.com>
2025-10-01 10:19:32 -0700

    Added comments and got rid of redundant logic

--- test_wikipedia_api.py
+++ test_wikipedia_api.py
@@ -1,44 +1,49 @@
 import requests
 import geoip2.database
+import ipaddress
 import csv
 
 url = "https://en.wikipedia.org/w/api.php"
+
+# Header to simulate user access
+headers = {"User-Agent": "WikipediaEditTracker/1.0 dboswell@chapman.edu"}
+
+# Param to navigate to, and access, 250 user IPs
 params = {
     "action": "query",
     "list": "recentchanges",
-    "rcprop": "title|user|timestamp|comment|flags",
+    "rcprop": "user",
     "rclimit": 250,
     "format": "json"
 }
 
-headers = {"User-Agent": "WikipediaEditTracker/1.0 dboswell@chapman.edu"}
-
+# Making request and storing data as json
 response = requests.get(url, params=params, headers=headers)
 data = response.json()
 
-# Load the database
+# Loading the database
 reader = geoip2.database.Reader("GeoLite2-Country.mmdb")
-user_ip = {}
 
+# Dictionary to hold IP and Country
+ip_country = {}
+
+# Iterating through all recent edits and finding unregistered accounts
 for change in data["query"]["recentchanges"]:
-    if change["user"][0].isdigit():
-        try:
-            ip = change["user"]
-            response = reader.country(ip)
-            user_ip[change['user']] = response.country.name
-            print(change['user'],"->",response.country.name)
-        except:
-            print("Not Real")
+    try:
+        ip = change["user"] # Getting IP
+        response = reader.country(ip) # Getting country from geoip2 database
+        ip_country[change['user']] = response.country.name # Storing IP and Country in dictionary
+        print(change['user'],"->",response.country.name)
+    except:
+        print("Registered Account") # In case we encounter an invalid IP/Username
 
-
+# Writing values to csv
 with open("user_ip_data.csv",'w',newline="") as csvFile:
     writer = csv.writer(csvFile)
-    for key, value in user_ip.items():
+    for key, value in ip_country.items():
         writer.writerow([key,value])
 
 csvFile.close()
-    
-
 reader.close()
 
 
