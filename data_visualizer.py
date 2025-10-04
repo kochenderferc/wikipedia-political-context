0d21a44ef38cbefadaa2f33bdd77d0c957d0ffc7
Dylan <dylboswell@gmail.com>
2025-10-03 20:35:29 -0700

    Better comments and updated figure caption

--- data_visualizer.py
+++ data_visualizer.py
@@ -5,22 +5,23 @@ import sys
 def read_csv(csv_file):
     try:
         country_edit_dict = {}
-        total_edit_count = 0
-        country_edit_dict_size = 0
         # Reading from the csv and creating a dictionary with their values
         with open(csv_file,'r') as csvFile:
             reader = csv.reader(csvFile,delimiter=",")
-            for edit in reader:
-                total_edit_count += 1
+            edits = list(reader) # creating a list of edits from the reader iterables
+            total_edit_count = len(edits)
+            start_date = edits[0][2].split()[0]
+            end_date = edits[total_edit_count-1][2].split()[0]
+
+            # Grabbing the country from edit to set as the key in a country:IP dict.
+            for edit in edits: 
                 country = edit[1]
                 if country in country_edit_dict:
                     country_edit_dict[country] = country_edit_dict[country] + 1
-                    country_edit_dict_size += 1
                 else:
                     country_edit_dict[country] = 1
-                    country_edit_dict_size += 1
-
-        return (country_edit_dict, total_edit_count, country_edit_dict_size)    
+          
+        return (country_edit_dict, total_edit_count,start_date,end_date)    
     except:
         print("\n\tERROR READING FROM CSV FILE: '",csv_file,"'\n\n")
         exit()
@@ -35,7 +36,9 @@ def print_country_edit_counts(csv_data):
 
 def plot_data(csv_data):
     country_ip_dict = csv_data[0]
-    total_edit_count = csv_data[2]
+    total_edit_count = csv_data[1]
+    start_date = csv_data[2]
+    end_date = csv_data[3]
     plt.figure(figsize=(10,6))  # making the figure wider to fit labels
 
     for key, value in country_ip_dict.items():
@@ -57,7 +60,7 @@ def plot_data(csv_data):
     plt.ylabel("Edit Counts")
     plt.title("Country vs Edit Counts")
     # Adding caption at the bottom
-    plt.figtext(0.5, 0.01, f"Figure 1:  Count of edits made to english Wikipedia from dataset of edits made by registered accounts, N={total_edit_count}", ha="center", fontsize=9, style="italic")
+    plt.figtext(0.5, 0.01, f"Figure 1:  Count of edits made to english Wikipedia by country from {start_date} to {end_date},N={total_edit_count}.", ha="center", fontsize=9, style="italic")
     plt.tight_layout()  # fitting labels
     plt.show()
 
@@ -74,7 +77,7 @@ def get_csv_dates(csv_data):
         csvFile.close()
         return dates
     except:
-        print("\n\tERROR READING FROM CSV FILE: '",csv_file,"'\n\n")
+        print("\n\tERROR READING FROM CSV FILE: '",csv_data,"'\n\n")
         exit()
         return None
 
