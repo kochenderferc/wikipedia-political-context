3ca550dd84b4b8cd32ef9637c91b39f2ca36db00
Dylan <dylboswell@gmail.com>
2025-10-21 10:59:28 -0700

    Created setup/install script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Exits on command failure
+set -e
+
+echo "Detected OS type: $OSTYPE"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Running On Linux"
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    cd src/
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Running On macOS"
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    cd src/
+elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
+    echo "Running on windows"
+    python -m venv venv
+    source venv/Scripts/activate
+    pip install -r requirements.txt
+    cd src/
+else
+    echo "Unknown OS: $OSTYPE"
+    exit 1
+fi
+
+echo "Setup Complete"
+
+
