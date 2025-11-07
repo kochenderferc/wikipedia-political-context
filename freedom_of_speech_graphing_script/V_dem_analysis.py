8d287feaa752b2aa4230d5ee3948d0950ed87e3d
Dylan <dylboswell@gmail.com>
2025-11-07 10:25:38 -0800

    Added freedom_speech_graph code changes

--- freedom_of_speech_graphing_script/V_dem_analysis.py
+++ freedom_of_speech_graphing_script/V_dem_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 # --- load data ---
 # update the file paths/URLs as appropriate
@@ -21,10 +22,33 @@ plt.figure(figsize=(8,6))
 plt.scatter(df3['v2x_libdem'], df3['v2x_freexp_altinf'], s=50)
 
 for i, row in df3.iterrows():
-    plt.text(row['v2x_libdem'] + 0.005, row['v2x_freexp_altinf'] + 0.005, row['country_text_id'], fontsize=9)
+    # Setting variables
+    x = row['v2x_libdem']
+    y = row['v2x_freexp_altinf']
+
+    # Getting values for plotting lines
+    r = math.sqrt(x**2 + y**2) # Magnitude/Length
+    m = y/x if x != 0 else float('inf') # Angle/Slope
+
+    # Computing deltas for line plotting
+    dx = r / math.sqrt(1 + m**2)
+    dy = m * dx
+
+    # Plotting Lines
+    end_point_x = [0, dx] # From origin to x
+    end_point_y = [0, dy] # From origin to y
+    plt.plot(end_point_x, end_point_y, 'o-',label=f"{row['country_text_id']} - Magnitude {r:.3f}") # Label param is for legend
+    
+    # Console Output
+    print('---')
+    print(row['country_text_id'])
+    print("x=",x, "y=",y)
+    print('Slope:', m)
+    print('Magnitude:', r)
 
 plt.xlabel('Liberal Democracy Index (0-1)')
 plt.ylabel('Freedom of Expression Index (0-1)')
-plt.title(f'Democracy vs Freedom of Expression – {year}')
+plt.title(f'Liberal Democracy vs Freedom of Expression – {year}')
+plt.legend()
 plt.grid(True)
 plt.show()
