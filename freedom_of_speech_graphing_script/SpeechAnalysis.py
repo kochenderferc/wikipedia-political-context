4dce2a5a06653ef59e9eb78fa165a6738aa778c2
Dylan <dylboswell@gmail.com>
2025-11-07 18:22:32 -0800

    Made class for running analysis

---
+++ freedom_of_speech_graphing_script/SpeechAnalysis.py
@@ -0,0 +1,66 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+class SpeechAnalysis:
+    def __init__ (self,csv_file_path):
+        # -- Processing To Get Country Speech and Expression Data --
+
+        # load csv
+        csv = pd.read_csv(csv_file_path, low_memory=False)
+
+        # Filter for year 2023
+        year_df = csv[csv['year'] == 2023]
+
+        # Filter for Freedom of Expression and Liberal Democracy variables
+        speech_df = year_df[['country_name', 'country_text_id', 'v2x_freexp_altinf', 'v2x_libdem']].dropna()
+
+        # Filter for specific countries
+        countries = ['United States', 'Canada', 'Germany', 'India', 'Brazil', 'China', 'Russia', 'South Africa', 'Japan', 'Australia']
+        self.country_df = speech_df[speech_df['country_name'].isin(countries)]
+
+    def get_country_data(self):
+        countries_list = []
+        for i, row in self.country_df.iterrows():
+            x = row['v2x_libdem']
+            y = row['v2x_freexp_altinf']
+            # Calculating Values
+            r = math.sqrt(x**2 + y**2) # Magnitude/Length
+            m = y/x if x != 0 else float('inf') # Angle/Slope
+            countries_list.append((row['country_name'], r, m))
+        return countries_list
+
+    def compute_delta(self,country_data):
+        country, r, m = country_data
+        dx = r / math.sqrt(1 + m**2)
+        dy = m * dx
+        return (country, dx, dy)
+
+    def plot_data(self,country_data):
+        country_name, dx, dy = self.compute_delta(country_data)
+        # Plotting Lines
+        end_point_x = [0, dx] # From origin to x
+        end_point_y = [0, dy] # From origin to y
+        plt.plot(end_point_x, end_point_y, 'o-',label=f"{country_name} - Magnitude {float(country_data[1]):.3f}") # Label param is for legend
+
+    def make_plot(self):
+        plt.figure(figsize=(8,6))
+        plt.scatter(self.country_df['v2x_libdem'], self.country_df['v2x_freexp_altinf'], s=50)
+
+        for country_data in self.get_country_data():
+            self.plot_data(country_data)
+        
+        plt.axis('equal')
+        plt.xlabel('Liberal Democracy Index (0-1)')
+        plt.ylabel('Freedom of Expression Index (0-1)')
+        plt.title(f'Liberal Democracy vs Freedom of Expression – {2023}')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+
+if __name__ == "__main__":
+    analysis = SpeechAnalysis('V-Dem-CY-Full+Others-v15.csv')
+    analysis.make_plot()
