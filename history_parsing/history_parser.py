1a8d384a2101e6a4176aaac63a3480d43beab244
Dylan <dylboswell@gmail.com>
2026-02-12 17:21:46 -0800

    adding starter work

--- history_parsing/history_parser.py
+++ history_parsing/history_parser.py
@@ -1,1 +1,14 @@
-print("history_parser")
+import glob
+import os
+import pandas as pd
+
+
+tsv_files = glob.glob("data/*.tsv")
+
+for file in tsv_files:
+    print(file)
+    df = pd.read_csv(file, sep="\t")
+
+
+
+print(df.columns)
